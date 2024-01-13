adee4463105bf28b6f0541d38aff45f70ea3f6a6
Chukwudi Eze <chukwudieze97@gmail.com>
2024-01-13 22:22:21 +0100

    adjust theme

---
+++ theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#23A6F0",
+    },
+    secondary: {
+      main: "#23856D",
+    },
+  },
+  typography: {
+    fontFamily: "Montserrat, sans-serif",
+  },
+});
